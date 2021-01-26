bd7bb8a29c0103905651bf2762ecd9a11d06d2dc
Dana Harrison <dana@dharrison.ca>
2021-01-26 16:09:28 -0500

    Working with height and global vars

--- resources/desk.py
+++ resources/desk.py
@@ -1,12 +1,18 @@
 from flask import Flask
 from flask_restful import Api, Resource, reqparse
 
+height = 0
 class Desk(Resource):
         def get(self):
                 return "Get method", 200
 
         def post(self, direction):
-		if direction.lower() == 'up' or direction.lower() == 'down':
-                        return f"Moved desk in {direction}.", 200
-		else:
+                global height
+                if direction.lower() == 'up':                        
+                        height += 50
+                        return f"Moved desk {direction} to {height}.", 200
+                elif direction.lower() == 'down':                        
+                        height -= 50
+                        return f"Moved desk {direction} to {height}.", 200
+                else:
                         return f"{direction} is not a valid direction.", 500
