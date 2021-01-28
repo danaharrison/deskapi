ed21df78b4e05b9d8b3002163729fe49f72710f4
Dana Harrison <dana@dharrison.ca>
2021-01-28 13:03:57 -0500

    Updated logic for updown

--- resources/desk.py
+++ resources/desk.py
@@ -18,51 +18,64 @@ class Desk(Resource):
     def get(self):
         return f"Height is {getHeight()}", 200
 
-    def post(self, direction):
+    def post(self):
         global height
 
         parser = reqparse.RequestParser()
-        parser.add_argument('pin', type=int, required=False)
-        parser.add_argument('state', type=int, required=False)
+        parser.add_argument('direction', type=str, required=False)
+        #parser.add_argument('state', type=int, required=False)
         args = parser.parse_args()
-        
-        GPIO.setmode(GPIO.BCM)
+
         gpioPin = -1
+        
+        if args['direction'] == 'up':
+            gpioPin = 17
+        elif args['direction'] == 'down':
+            gpioPin = 18
+
+        GPIO.setmode(GPIO.BCM)        
         gpioState = GPIO.LOW
+
+        GPIO.setup(gpioPin, GPIO.OUT)
+        GPIO.output(gpioPin, GPIO.HIGH)
+        time.sleep(5)
+        GPIO.output(gpioPin, GPIO.LOW)
+
         
         if args['pin'] == 0 or args['pin'] == 1 and args['state'] == 0 or args['state'] == 1:
-            if args['pin'] == 0:
-                gpioPin = 17
-            elif args['pin'] == 1:
-                gpioPin = 18
+            # if args['pin'] == 0:
+            #     gpioPin = 17
+            # elif args['pin'] == 1:
+            #     gpioPin = 18
 
-            if args['state'] == 1:
-                gpioState = GPIO.HIGH
+            # if args['state'] == 1:
+            #     gpioState = GPIO.HIGH
 
-            if gpioPin != -1:
-                GPIO.setup(gpioPin, GPIO.OUT)
-                GPIO.output(gpioPin, gpioState)
-                print(f"GPIO state is {GPIO.input(gpioPin)}")
+            # if gpioPin != -1:
+            #     GPIO.setup(gpioPin, GPIO.OUT)
+            #     GPIO.output(gpioPin, gpioState)
+            #     print(f"GPIO state is {GPIO.input(gpioPin)}")
 
-            if direction.lower() == 'up':
-                if height >= maxHeight:
-                    return "Already at max height.", 500
-                else:
-                    while height < maxHeight:
-                        # Standing height here
-                        height += 1
-                        # Fire relay up here
-                    return f"Moved desk {direction} to {height}.", 200
-            elif direction.lower() == 'down':
-                if height <= minHeight:
-                    return "Already at min height.", 500
-                else:
-                    while height > minHeight:
-                        # Sitting height here
-                        height -= 1
-                        # Fire relay down here
-                    return f"Moved desk {direction} to {height}.", 200
-            else:
-                return f"{direction} is not a valid direction.", 500
+            # if direction.lower() == 'up':
+            #     if height >= maxHeight:
+            #         return "Already at max height.", 500
+            #     else:
+            #         while height < maxHeight:
+            #             # Standing height here
+            #             height += 1
+            #             # Fire relay up here
+            #         return f"Moved desk {direction} to {height}.", 200
+            # elif direction.lower() == 'down':
+            #     if height <= minHeight:
+            #         return "Already at min height.", 500
+            #     else:
+            #         while height > minHeight:
+            #             # Sitting height here
+            #             height -= 1
+            #             # Fire relay down here
+            #         return f"Moved desk {direction} to {height}.", 200
+            # else:
+            #     return f"{direction} is not a valid direction.", 500
+            return "Hello", 200
         else:
             return "Invalid pin or state value.", 500
