ff92e417e5fef7f2f64fa635b9fa8bd59e3e978c
Dana Harrison <dana@dharrison.ca>
2021-01-29 15:42:53 -0500

    LED colour and brightness endpoints

--- resources/light.py
+++ resources/light.py
@@ -5,23 +5,31 @@ import neopixel
 import time
 import webcolors
 
+
 class Light(Resource):
     def post(self):
         parser = reqparse.RequestParser()
-        parser.add_argument('colour', type=str, required=True)
+        parser.add_argument('colour', type=str, required=False)
+        parser.add_argument('brightness', type=str, required=False)
         args = parser.parse_args()
-        colour = args['colour']
-        print(colour)
+        brightness = 0.5
 
-        pixels = neopixel.NeoPixel(board.D21, 60, brightness=0.2)
-        
-        #pixels.setBrightness(50)
-        pixels.fill(webcolors.name_to_rgb(colour))
-        time.sleep(2)
-        #pixels.fill((255, 0, 0))
-        #time.sleep(2)
-        #pixels.fill((0, 0, 255))
-        #time.sleep(2)
-        pixels.fill((255, 255, 255))
-        time.sleep(2)
-        pixels.deinit()
+        colour = args['colour']
+        if args['brightness'] is not None:
+            brightnessArg = float(args['brightness'])
+            if 0.10 <= brightnessArg <= 1.00:
+                brightness = brightnessArg
+
+        #print( f"Colour is {colour} and brightness is {brightness}" )
+        pixels = neopixel.NeoPixel(board.D21, 60, brightness=brightness)
+        #print( f"Brightness argument is {brightnessArg}")
+
+        #pixels = neopixel.NeoPixel(board.D21, 60, brightness = 0.2)
+        #print(colour)
+        if colour is not None:
+            if colour == 'off':
+                pixels.deinit()
+            else:
+                pixels.fill(webcolors.name_to_rgb(colour))
+        else:
+            return "No colour specified.", 400
