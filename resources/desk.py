128689e6814605f938e6339fc1972731e6b2ea02
Dana Harrison <dana@dharrison.ca>
2021-01-31 16:50:31 -0500

    Pulled display logic into desk endpoint

--- resources/desk.py
+++ resources/desk.py
@@ -3,6 +3,11 @@ from flask_restful import Api, Resource, reqparse
 from multiprocessing import Process
 import RPi.GPIO as GPIO
 import time
+import subprocess
+from board import SCL, SDA
+import busio
+from PIL import Image, ImageDraw, ImageFont
+import adafruit_ssd1306
 
 maxHeight = 125
 minHeight = 50
@@ -37,12 +42,51 @@ def getHeight():
         distance = round(pulse_duration * 17150, 2)
 
         print(f"Distance: {distance} cm")
+        showHeight(distance)
 
     finally:
         #GPIO.cleanup()
         return distance
 
-def showHeight(heightValue)
+def showHeight(heightValue):
+    # Create the I2C interface.
+    i2c = busio.I2C(SCL, SDA)
+
+    # Create the SSD1306 OLED class.
+    # The first two parameters are the pixel width and pixel height.  Change these
+    # to the right size for your display!
+    disp = adafruit_ssd1306.SSD1306_I2C(128, 32, i2c)
+
+    # Clear display.
+    disp.fill(0)
+    disp.show()
+
+    # Create blank image for drawing.
+    # Make sure to create image with mode '1' for 1-bit color.
+    width = disp.width
+    height = disp.height
+    image = Image.new("1", (width, height))
+
+    # Get drawing object to draw on image.
+    draw = ImageDraw.Draw(image)
+
+    # Draw a black filled box to clear the image.
+    draw.rectangle((0, 0, width, height), outline=0, fill=0)
+
+    # Draw some shapes.
+    # First define some constants to allow easy resizing of shapes.
+    padding = -2
+    top = padding
+    bottom = height - padding
+    # Move left to right keeping track of the current x position for drawing shapes.
+    x = 0
+
+    font = ImageFont.truetype('VCR_OSD_MONO_1.001.ttf', 30)
+
+    draw.text((x, top + 0), f"{heightValue} cm", font=font, fill=255, align="center")
+
+    disp.image(image)
+    disp.show()
 
 def moveDesk(direction):
     gpioPin = -1
